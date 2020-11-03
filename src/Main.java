e9f60911368e75ca14f1f016f8141be6ebc21033
Joe Gracey <joe.gracey@gmail.com>
2020-11-02 20:00:09 -0700

    started ending if statement

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
-//import java.util.Scanner;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scannerObject = new Scanner(System.in);
+
         String[] madLibWords = new String[] {"boys", "girls", "lemonade", "fuzzy", "squeak",
                 "snort", "hungry", "mouse", "blue-fin tuna"};
-
-	    /*
-	    Zoos are places where :plural_noun are kept in pens or cages :!
-	    so that :plural_noun can come and look at them :. There is a zoo :!
-	    in the park beside the :type_of_liquid fountain :. When it is feeding time :, :!
-	    all the animals make :adjective noises. The elephant goes :< :funny_noise :> :!
-	    and the turtledoves go :< :another_funny_noise :. :> My favorite animal is the :!
-	    :adjective :animal :, so fast it can outrun a/an :another_animal :. :!
-	    You never know what you will find at the zoo :.
-
-	    Do you want to play again (y/n)? n
-	    Thank you for playing.
-
-	    :! = newline, :, = comma, :. = period, :< :> = quotes
-	     */
-
-        //Scanner scannerObject = new Scanner(System.in);
-
-        System.out.println();
 
         System.out.println("Zoos are places where wild " + madLibWords[0] + " are kept in pens or" +
                 " " + "cages\nso that " + madLibWords[1] + " can come and look at them. There is " +
@@ -36,8 +20,18 @@ public class Main {
                 "what you will find at the zoo.");
 
         System.out.println(" ");
+
         System.out.println("Do you want to play again (y/n)?");
-        System.out.println("Thank you for playing.");
+        String playAgain = scannerObject.nextLine();
+
+        if(playAgain.equals("y") || playAgain.equals("Y")) {
+            //print out paragraph again
+
+        } else if (playAgain.equals("n") || playAgain.equals("N")){
+            System.out.println("Thank you for playing.");
+        } else {
+            System.out.println("Please enter Y or N");
+        }
     }
 
 
