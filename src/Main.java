d7b31b2caf012c8c818eb9a0bf6c1c6710cc6f40
Joe Gracey <joe.gracey@gmail.com>
2020-11-02 18:40:07 -0700

    created string array

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        String[] madLibWords = new String[] {"boys", "girls", "lemonade", "fuzzy", "squeak",
+                "snort", "hungry", "mouse", "blue-fin tuna"};
+
 	    /*
 	    Zoos are places where :plural_noun are kept in pens or cages :!
 	    so that :plural_noun can come and look at them :. There is a zoo :!
@@ -20,30 +24,21 @@ public class Main {
 
         //Scanner scannerObject = new Scanner(System.in);
 
-        String plural_noun = "boys";
-        String another_plural_noun = "girls";
-        String type_of_liquid = "lemonade";
-        String adjective = "fuzzy";
-        String funny_noise = "squeak";
-        String another_funny_noise = "snort";
-        String another_adjective = "hungry";
-        String animal = "mouse";
-        String another_animal = "blue-fin tuna";
-        charhar playAgain = 'y';
+        System.out.println();
 
-        System.out.println("Zoos are places where wild " + plural_noun + " are kept in pens or " +
-                "cages\nso that " + another_plural_noun + " can come and look at them. There is a" +
-                " zoo\nin the park beside the " + type_of_liquid + " fountain. When it is " +
-                "feeding time,\nall the animals make " + adjective + " noises. The elephant goes " + "\"" +
-                funny_noise + "\"" + "\nand the turtledoves go " + "\"" + another_funny_noise +
-                "\"" + ". My favorite animal is the\n" + another_adjective + " " + animal + ", so fast it can outrun " +
-                "a/an " + another_animal + ".\nYou never know what you will find at the zoo.");
+        System.out.println("Zoos are places where wild " + madLibWords[0] + " are kept in pens or" +
+                " " + "cages\nso that " + madLibWords[1] + " can come and look at them. There is " +
+                "a" + " zoo\nin the park beside the " + madLibWords[2] + " fountain. When it is " +
+                "feeding time,\nall the animals make " + madLibWords[3] + " noises. The elephant " +
+                "goes " + "\"" + madLibWords[4] + "\"" + "\nand the turtledoves go " + "\"" + madLibWords[5] +
+                "\"" + ". My favorite animal is the\n" + madLibWords[6] + " " + madLibWords[7] +
+                ", so " + "fast it can outrun " + "a/an " + madLibWords[8] + ".\nYou never know " +
+                "what you will find at the zoo.");
 
-        if() {
-
-        }
         System.out.println(" ");
         System.out.println("Do you want to play again (y/n)?");
         System.out.println("Thank you for playing.");
     }
+
+
 }
