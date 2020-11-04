118713ad8a9c93e2460e769ff36f081864f11938
Joe Gracey <joe.gracey@gmail.com>
2020-11-03 20:21:39 -0700

    added imports

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,21 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        madLibs();
-    }
+        File fileObject = new File("C:/Users/josep/Desktop/doc.txt");
 
-    public static void madLibs() {
-        //Scanner scannerObject = new Scanner(System.in);
-
-        String[] madLibWords = new String[] {"boys", "girls", "lemonade", "fuzzy", "squeak",
-                "snort", "hungry", "mouse", "blue-fin tuna"};
-
-        System.out.println("Zoos are places where wild " + madLibWords[0] + " are kept in pens or" +
-                " " + "cages\nso that " + madLibWords[1] + " can come and look at them. There is " +
-                "a" + " zoo\nin the park beside the " + madLibWords[2] + " fountain. When it is " +
-                "feeding time,\nall the animals make " + madLibWords[3] + " noises. The elephant " +
-                "goes " + "\"" + madLibWords[4] + "\"" + "\nand the turtledoves go " + "\"" + madLibWords[5] +
-                "\"" + ". My favorite animal is the\n" + madLibWords[6] + " " + madLibWords[7] +
-                ", so " + "fast it can outrun " + "a/an " + madLibWords[8] + ".\nYou never know " +
-                "what you will find at the zoo.");
-
-        System.out.println(" ");
-        playAgain();
-
-    }
-
-    public static void playAgain() {
-        Scanner scannerObject = new Scanner(System.in);
-
-        System.out.println(" ");
-        System.out.print("Do you want to play again (y/n)? ");
-        String yesOrNo = scannerObject.nextLine();
-        System.out.println(" ");
-
-        if(yesOrNo.equals("y") || yesOrNo.equals("Y")) {
-            madLibs();
-        } else if (yesOrNo.equals("n") || yesOrNo.equals("N")){
-            System.out.println("Thank you for playing.");
-        } else {
-            System.out.print("Please enter a 'y' or a 'n' ");
-            System.out.println(" ");
-            playAgain();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileObject));
+            bufferedReader.lines().forEach(System.out::println);
         }
+        catch(IOException exception) {
+            exception.printStackTrace();
+        }
+
     }
 
 }
