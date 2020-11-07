6ac0da9ad3bd5d10f2bb97a26dd2dadaf6f31909
Joe Gracey <joe.gracey@gmail.com>
2020-11-06 19:10:04 -0700

    added questions

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,73 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        File fileObject = new File("C:/Users/josep/Desktop/doc.txt");
+	    String theString;
+        File fileObject = new File("src/" + getFileName());
+        Scanner scanner = null;
 
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileObject));
-            bufferedReader.lines().forEach(System.out::println);
-        }
-        catch(IOException exception) {
+            scanner = new Scanner(fileObject);
+        } catch(FileNotFoundException exception) {
             exception.printStackTrace();
         }
 
+        theString = scanner.nextLine();
+
+        while(scanner.hasNextLine()) {
+            theString = theString + "\n" + scanner.nextLine();
+        }
+
+        char[] madLibArray = theString.toCharArray();
+
+        try {
+            questions(madLibArray);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileObject));
+            bufferedReader.lines().forEach(System.out::println);
+        } catch(IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
+    private static String getFileName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        return scanner.next();
+    }
+
+    private static void questions(char[] madLibArray) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the following:");
+
+        System.out.print("Plural noun: ");
+        String pluralNoun = scanner.nextLine();
+
+        System.out.print("Another plural noun: ");
+        String anotherPluralNoun = scanner.nextLine();
+
+        System.out.print("Type of liquid: ");
+        String typeOfLiquid = scanner.nextLine();
+
+        System.out.print("Adjective: ");
+        String adjective = scanner.nextLine();
+
+        System.out.print("Funny noise: ");
+        String funnyNoise = scanner.nextLine();
+
+        System.out.print("Another funny noise: ");
+        String anotherFunnyNoise = scanner.nextLine();
+
+        System.out.print("Another adjective: ");
+        String anotherAdjective = scanner.nextLine();
+
+        System.out.print("Animal: ");
+        String animal = scanner.nextLine();
+
+        System.out.print("Another animal: ");
+        String anotherAnimal = scanner.nextLine();
+
+    }
 }
